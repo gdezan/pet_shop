aa53301ed8e0cfecc711bf791bbe4dcf6756bfa5
gdezan <33992304+gdezan@users.noreply.github.com>
2019-09-16 20:37:04 -0300

    finishing touches for the landing page

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ const testProduct = {
   href: "#",
 };
 
-for (let i = 0; i < 20; i++) products.push(testProduct);
+for (let i = 0; i < 24; i++) products.push(testProduct);
 
 products.forEach(product => {
   productList.innerHTML += `
-    <div class="card">
-      <img src="${product.img}" alt="dog food" class="promo-card-img" />
-      <div class="product-price">
-        <div class="card-product">${product.product}</div>
-        <div class="card-price">${product.price}</div>
-      </div>
+    <a href="${product.href}" class="card">
+    <img src="${product.img}" alt="dog food" class="promo-card-img" />
+    <div class="product-price">
+    <div class="card-product">${product.product}</div>
+    <div class="card-price">${product.price}</div>
     </div>
+    </a>
   `;
 });
