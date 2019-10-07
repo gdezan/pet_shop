6c8c8665d404f52ba04659f5c3d1db007e07389a
Gabriel Dezan <dezangabriel@gmail.com>
2019-10-07 17:49:57 -0300

    add router and workspace configs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Router } from "@reach/router";
+
+const Home = () => {
+  return <Link to="dashboard">Dashboard</Link>;
+};
+const Dashboard = () => {
+  return <Link to="/">Home</Link>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Home path="/" />
+      <Dashboard path="/dashboard" />
+    </Router>
   );
 }
 
